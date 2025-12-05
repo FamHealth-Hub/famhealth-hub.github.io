7be1e4eb2f1e2b2d92d73cfbbb162feddc5d3485
Julian Kaiser <jkai-200211@outlook.de>
2025-12-05 15:59:53 +0100

    Verbesserungen der vorhandenen Seiten

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,24 +1,14 @@
 const auth = {
-  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  // DEINE PIN – ÄNDERE HIER, WENN DU WILLST!
   CORRECT_PIN: "200211",
-  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
   login() {
     const input = document.getElementById('pin-input').value;
-
     if (input === this.CORRECT_PIN) {
       sessionStorage.setItem('unlocked', 'true');
       window.location.href = 'app.html';
     } else {
       document.getElementById('error-msg').textContent = 'Falsche PIN';
       document.getElementById('pin-input').value = '';
-    }
-  },
-
-  checkIfUnlocked() {
-    if (sessionStorage.getItem('unlocked') === 'true') {
-      window.location.href = 'app.html';
     }
   }
 };
