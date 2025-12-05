d37b9993dc10808a49237632686393e1cb39116c
Julian Kaiser <jkai-200211@outlook.de>
2025-12-05 17:02:44 +0100

    Neue Funktionen (Patientenanlage per JS, Speicherung in DB) sowie Bugfixes und Style-Improvements

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,44 +1,39 @@
+// assets/js/db.js
 const DB_NAME = "FamHealthHub";
-const DB_VERSION = 1;
-
+const DB_VERSION = 2;
 let db;
 
-const initDB = () => {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DB_NAME, DB_VERSION);
+const initDB = () => new Promise((resolve, reject) => {
+  const req = indexedDB.open(DB_NAME, DB_VERSION);
 
-    request.onerror = () => reject(request.error);
-    request.onsuccess = () => {
-      db = request.result;
-      resolve(db);
-    };
+  req.onupgradeneeded = e => {
+    db = e.target.result;
+    db.createObjectStore("patients", { keyPath: "id", autoIncrement: true });
+    db.createObjectStore("vitals",   { keyPath: "id", autoIncrement: true });
+    db.createObjectStore("exams",    { keyPath: "id", autoIncrement: true });
+  };
 
-    request.onupgradeneeded = (e) => {
-      db = e.target.result;
-      db.createObjectStore("data", { keyPath: "id" });
-    };
-  });
+  req.onsuccess = () => { db = req.result; resolve(db); };
+  req.onerror = () => reject(req.error);
+});
+
+const getStore = (name, mode = "readonly") => {
+  const tx = db.transaction(name, mode);
+  return tx.objectStore(name);
 };
 
-const getStore = (mode = "readonly") => {
-  const tx = db.transaction("data", mode);
-  return tx.objectStore("data");
-};
+// Patienten
+const getAllPatients = () => new Promise(res => {
+  const store = getStore("patients");
+  const req = store.getAll();
+  req.onsuccess = () => res(req.result);
+});
 
-const get = (key) => {
-  return new Promise((resolve) => {
-    const store = getStore();
-    const req = store.get(key);
-    req.onsuccess = () => resolve(req.result?.value);
-  });
-};
+const addPatient = patient => new Promise(res => {
+  const store = getStore("patients", "readwrite");
+  const req = store.add(patient);
+  req.onsuccess = () => res(req.result);
+});
 
-const set = (key, value) => {
-  return new Promise((resolve) => {
-    const store = getStore("readwrite");
-    store.put({id: key, value});
-    resolve();
-  });
-};
-
+// Vitals & Exams kommen in Phase 5/6
 initDB();
